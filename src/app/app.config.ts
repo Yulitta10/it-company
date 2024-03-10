fd6024f5f407caf8c9ffe6ddf3a441bc9097684e
yulitta_demyanovich <yulitta06102000@gmail.com>
2024-03-10 22:13:37 +0300

    added header for page

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,11 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 import { NgxsModule } from '@ngxs/store';
 import { DefaultState } from '../state/default.state';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(NgxsModule.forRoot([DefaultState]))]
+  providers: [provideRouter(routes), provideAnimations(), importProvidersFrom(NgxsModule.forRoot([DefaultState]))]
 };
